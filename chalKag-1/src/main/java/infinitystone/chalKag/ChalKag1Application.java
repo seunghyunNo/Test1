db007ce59e41a4fd65dfc07cefd448f4fe503d71
seunghyunNo <138773698+seunghyunNo@users.noreply.github.com>
2024-06-13 18:56:29 +0900

    Add files via upload

---
+++ chalKag-1/src/main/java/infinitystone/chalKag/ChalKag1Application.java
@@ -0,0 +1,13 @@
+package infinitystone.chalKag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalKag1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalKag1Application.class, args);
+	}
+
+}
---
+++ chalKag-1/src/test/java/infinitystone/chalKag/ChalKag1ApplicationTests.java
@@ -0,0 +1,13 @@
+package infinitystone.chalKag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalKag1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
